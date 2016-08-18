190b548e8ef68ee326a7c6004c6938c18d04f519
Jz <coder_jz@163.com>
2016-08-18 22:42:24 +0800

    修改routers代码

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,9 @@
 import Router from 'koa-router'
 import home from '../controllers/home'
 
-const router = Router()
+const router = Router({
+  prefix: '/'
+})
 router.get('/', home.index)
 router.get('about', home.about)
 export default router
